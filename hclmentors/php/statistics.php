eff21dc0abae57db0d908e275a67b3ddc50d38be
AllanMoorhouse23 <allanmoorhouse.am@gmail.com>
2018-10-05 19:48:25 +1300

    Added first SQL query

--- hclmentors/php/statistics.php
+++ hclmentors/php/statistics.php
@@ -1,4 +1,5 @@
 <?php
+require_once('connect.php');
 include 'header.php';
 include 'navbar.php';
 ?>
@@ -12,6 +13,17 @@ Statistics
 Some random stuff
 </h1>
 
+<?php
+
+//NUMBER OF CLIENTS PER MONTH
+$query = "Select DATE_FORMAT(`date`, '%Y-%m') AS 'month', count(*) AS 'clientNumbers' from sessions group by MONTH(`date`)";
+$result = $con->query($query);
+foreach($result as $row){
+	echo $row['month'] . " " . $row['clientNumbers'] . "<br>";
+}
+
+
+?>
 
 
 
