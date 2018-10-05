808c0bac0297c1ddbc87f1af565ea9288c296f17
Ashton Lye <ashton@h189-072.fsi.waikato.ac.nz>
2018-10-05 13:39:59 +1300

    added new files from master

--- hclmentors/php/connect.php
+++ hclmentors/php/connect.php
@@ -1,7 +1,7 @@
 <?php
     try{
 		$con = new PDO('mysql:host=localhost;dbname=hcl_mentors','root','root');
-		//uni - $con = new PDO('mysql:host=mysql.cms.waikato.ac.nz;dbname=al123','al123','my11039762sql');
+		//uni - $con = new PDO('mysql:host=mysql.cms.waikato.ac.nz;dbname=','','');
 		//lappy - $con = new PDO('mysql:host=localhost;dbname=comp333-assn1-im','root','root');
    	} catch (PDOException $e) {
    		echo "Database connection error " . $e->getMessage();
