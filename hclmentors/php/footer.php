4e4e784ede1bb8c0d1a61a38772f8f16f69cb36f
AllanMoorhouse23 <allanmoorhouse.am@gmail.com>
2018-10-06 13:43:38 +1300

    Added footer logo and styling

--- hclmentors/php/footer.php
+++ hclmentors/php/footer.php
@@ -1,5 +1,8 @@
 		<footer>
-		Some random stuff in the footer
+		<div class="footer">
+			Some random stuff in the footer
+			<img src="../images/footerlogo.png" alt="Hamilton City Council logo">
+		</div>
 		</footer>
 	</body>
 </html>
