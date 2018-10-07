2e1154816c64db33df505fd9f0da10ddc9a435bb
AllanMoorhouse23 <allanmoorhouse.am@gmail.com>
2018-10-07 14:41:26 +1300

    Changed the main SQL query

--- hclmentors/php/statistics.php
+++ hclmentors/php/statistics.php
@@ -4,40 +4,41 @@ include 'header.php';
 include 'navbar.php';
 ?>
 
-<div class="pageContent">
-
-
+<div class="statistics">
 
 <h1>
 Statistics
 </h1>
 
 
-
-<table>
 <?php
+echo date("Y");
 //NUMBER OF CLIENTS PER MONTH THIS YEAR
-echo "<tr><th>Clients per month (". date("Y") .")</th></tr>";
-$query = "Select DATE_FORMAT(`date`, '%M') AS 'month', count(*) AS 'clientNumbers' from sessions WHERE YEAR(`date`) = YEAR(CURRENT_DATE) group by MONTH(`date`) ORDER BY 'month' ASC";
+echo "<table><tr><th>Month</th><th>Booked</th><th>Walkins</th><th>No Shows</th><th>Total</th></tr>";
+$query = "Select DATE_FORMAT(`date`, '%M') AS 'Month', count(IF(`walkin` = 'false' AND `noshow` = 'false', 1, NULL)) as 'Booked' ,count(IF(`walkin` = 'true', 1, NULL)) as 'Walkins', count(IF(`noshow` = 'true', 1, NULL)) as 'No Shows', count(*) AS 'Total' from sessions WHERE YEAR(`date`) = YEAR(CURRENT_DATE) group by MONTH(`date`) ORDER BY Month(`date`)";
 $result = $con->query($query);
 foreach($result as $row){
-	echo "<tr><td>".$row['month'] . "</td><td>" . $row['clientNumbers'] . "</td></tr>";
+	echo "<tr><td>".$row['Month'] . "</td><td>" . $row['Booked'] . "</td><td>".$row['Walkins']."</td><td>".$row['No Shows']."</td><td>".$row['Total']."</td></tr>";
 }
+echo "</table>";
 ?>
-</table>
 
-
-<table>
 <?php
-//NUMBER OF CLIENTS PER MONTH PREVIOUS YEAR
-echo "<tr><th>Clients per month (". date("Y", strtotime('-1 years')) .")</th></tr>";
-$query = "Select DATE_FORMAT(`date`, '%M') AS 'month', count(*) AS 'clientNumbers' from sessions WHERE YEAR(`date`) = YEAR(CURRENT_DATE - INTERVAL 1 YEAR) group by MONTH(`date`) ORDER BY 'month' ASC";
+echo date("Y", strtotime('-1 years'));
+
+echo "<table><tr><th>Month</th><th>Booked</th><th>Walkins</th><th>No Shows</th><th>Total</th></tr>";
+
+$query = "Select DATE_FORMAT(`date`, '%M') AS 'Month', count(IF(`walkin` = 'false' AND `noshow` = 'false', 1, NULL)) as 'Booked' ,count(IF(`walkin` = 'true', 1, NULL)) as 'Walkins', count(IF(`noshow` = 'true', 1, NULL)) as 'No Shows', count(*) AS 'Total' from sessions WHERE YEAR(`date`) = YEAR(CURRENT_DATE - INTERVAL 1 YEAR) group by MONTH(`date`) ORDER BY Month(`date`)";
+
 $result = $con->query($query);
+
 foreach($result as $row){
-	echo "<tr><td>".$row['month'] . "</td><td>" . $row['clientNumbers'] . "</td></tr>";
+	echo "<tr><td>".$row['Month'] . "</td><td>" . $row['Booked'] . "</td><td>".$row['Walkins']."</td><td>".$row['No Shows']."</td><td>".$row['Total']."</td></tr>";
 }
+
+echo "</table>";
 ?>
-</table>
+
 
 <table>
 <?php
