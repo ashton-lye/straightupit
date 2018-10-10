ccb424c416a63f91d1f4c1e1197991a116c67f39
AllanMoorhouse23 <allanmoorhouse.am@gmail.com>
2018-10-11 10:59:19 +1300

    Last panel finished off

--- hclmentors/statistics.php
+++ hclmentors/statistics.php
@@ -11,7 +11,7 @@ Statistics
 </h1>
 <div class="mainArea">
 <?php
-//NUMBER OF CLIENTS PER MONTH THIS YEAR
+//CLIENTS PANEL
 //echo basename($_SERVER['PHP_SELF']);
 echo '<div class="panel"><div class="innerPanel"><h2>Clients</h2><h3>'.date("Y")."</h3>";
 echo "<table><tr><th>Month</th><th>Booked</th><th>Walkins</th><th>No Shows</th><th>Total</th></tr>";
@@ -30,20 +30,17 @@ echo "<table><tr><th>Month</th><th>Booked</th><th>Walkins</th><th>No Shows</th><
 $query = "Select DATE_FORMAT(`date`, '%M') AS 'Month', count(IF(`walkin` = 'false' AND `noshow` = 'false', 1, NULL)) as 'Booked' ,count(IF(`walkin` = 'true', 1, NULL)) as 'Walkins', count(IF(`noshow` = 'true', 1, NULL)) as 'No Shows', count(*) AS 'Total' from sessions WHERE YEAR(`date`) = YEAR(CURRENT_DATE - INTERVAL 1 YEAR) group by MONTH(`date`) ORDER BY Month(`date`)";
 
 $result = $con->query($query);
-
 foreach($result as $row){
 	echo "<tr><td>".$row['Month'] . "</td><td>" . $row['Booked'] . "</td><td>".$row['Walkins']."</td><td>".$row['No Shows']."</td><td>".$row['Total']."</td></tr>";
 }
-
 echo "</table></div></div>";
-?>
 
 
-
-<?php
+//PLATFORMS PANEL
 echo '<div class="panel"><div class="innerPanel"><h2>Platforms</h2><h3>'.date("Y")."</h3><table>";
 $query = "SELECT `platform`, COUNT(`platform`) AS 'count' FROM `sessions` WHERE `noshow` = 'false' AND `platform` IS NOT NULL AND YEAR(`date`) = YEAR(CURRENT_DATE) GROUP BY `platform`";
 $result = $con->query($query);
+echo "<tr><th>Platforms</th><th>Total</th></tr>";
 foreach($result as $row){
 	echo "<tr><td>".ucfirst($row['platform']) . "</td><td>" . $row['count'] . "</td></tr>";
 }
@@ -52,6 +49,8 @@ echo "</table>";
 echo '<h3>'.date("Y", strtotime('-1 Years'))."</h3><table>";
 $query = "SELECT `platform`, COUNT(`platform`) AS 'count' FROM `sessions` WHERE `noshow` = 'false' AND `platform` IS NOT NULL AND YEAR(`date`) = YEAR(CURRENT_DATE - INTERVAL 1 YEAR) GROUP BY `platform`";
 $result = $con->query($query);
+
+echo "<tr><th>Platforms</th><th>Total</th></tr>";
 foreach($result as $row){
 	echo "<tr><td>".ucfirst($row['platform']) . "</td><td>" . $row['count'] . "</td></tr>";
 }
@@ -59,19 +58,66 @@ echo "</table></div></div>";
 ?>
 </div>
 
-<div class="mainArea">
 
+
+
+<div class="mainArea">
 <?php
-echo '<div class="panel"><div class="innerPanel"><h2>Mentors</h2><table>';
-$query = "Select sessions.mentor, mentors.active, mentors.fname, mentors.lname, count(IF(sessions.walkin = 'false' AND sessions.noshow = 'false', 1, NULL)) as 'Booked' ,count(IF(sessions.walkin = 'true', 1, NULL)) as 'Walkins', count(IF(sessions.noshow = 'true', 1, NULL)) as 'No Shows', count(*) AS 'Total' from sessions JOIN mentors ON sessions.mentor = mentors.username group by sessions.mentor ORDER BY mentors.active DESC";
+//Mentors Panel
+echo '<div class="panel"><div class="innerPanel"><h2>Mentors</h2><h3>Current Mentors</h3><table>';
+$query = "Select sessions.mentor, mentors.active, mentors.fname, mentors.lname, count(IF(sessions.walkin = 'false' AND sessions.noshow = 'false', 1, NULL)) as 'Booked' ,count(IF(sessions.walkin = 'true', 1, NULL)) as 'Walkins', count(IF(sessions.noshow = 'true', 1, NULL)) as 'No Shows', count(*) AS 'Total' from sessions JOIN mentors ON sessions.mentor = mentors.username WHERE mentors.active = 'true' group by sessions.mentor ORDER BY mentors.active DESC";
 $result = $con->query($query);
+
+echo "<tr><th>Mentor</th><th>Booked</th><th>Walkins</th><th>No Shows</th><th>Total</th></tr>";
+
 foreach($result as $row){
 	echo "<tr><td>".ucfirst($row['fname'])." ".ucfirst($row['lname']) ."</td><td>". $row['Booked'] . "</td><td>".$row['Walkins']."</td><td>".$row['No Shows']."</td><td>".$row['Total'] ."</td></tr>";
 }
-echo "</table></div></div>";
+
+
+echo '</table><h3>Previous Mentors</h3><table>';
+
+$query = "Select sessions.mentor, mentors.active, mentors.fname, mentors.lname, count(IF(sessions.walkin = 'false' AND sessions.noshow = 'false', 1, NULL)) as 'Booked' ,count(IF(sessions.walkin = 'true', 1, NULL)) as 'Walkins', count(IF(sessions.noshow = 'true', 1, NULL)) as 'No Shows', count(*) AS 'Total' from sessions JOIN mentors ON sessions.mentor = mentors.username WHERE mentors.active = 'false' group by sessions.mentor ORDER BY mentors.active DESC";
+$result = $con->query($query);
+
+echo "<tr><th>Mentor</th><th>Booked</th><th>Walkins</th><th>No Shows</th><th>Total</th></tr>";
+
+foreach($result as $row){
+	echo "<tr><td>".ucfirst($row['fname'])." ".ucfirst($row['lname']) ."</td><td>". $row['Booked'] . "</td><td>".$row['Walkins']."</td><td>".$row['No Shows']."</td><td>".$row['Total'] ."</td></tr>";
+}
+echo'</table></div></div>';
+
+
+
+
+//Libraries Panel
+echo '<div class="panel"><div class="innerPanel"><h2>Libraries</h2><h3>'.date("Y").'</h3><table>';
+$query = "Select sessions.library, count(IF(sessions.walkin = 'false' AND sessions.noshow = 'false', 1, NULL)) as 'Booked' ,count(IF(sessions.walkin = 'true', 1, NULL)) as 'Walkins', count(IF(sessions.noshow = 'true', 1, NULL)) as 'No Shows', count(*) AS 'Total' from sessions WHERE YEAR(`date`) = YEAR(CURRENT_DATE) group by sessions.library";
+$result = $con->query($query);
+
+echo "<tr><th>Library</th><th>Booked</th><th>Walkins</th><th>No Shows</th><th>Total</th></tr>";
+
+foreach($result as $row){
+	echo "<tr><td>".ucfirst($row['library'])."</td><td>". $row['Booked'] . "</td><td>".$row['Walkins']."</td><td>".$row['No Shows']."</td><td>".$row['Total'] ."</td></tr>";
+}
+echo'</table>';
+
+echo '<h3>'.date("Y", strtotime('-1 years')).'</h3><table>';
+$query = "Select sessions.library, count(IF(sessions.walkin = 'false' AND sessions.noshow = 'false', 1, NULL)) as 'Booked' ,count(IF(sessions.walkin = 'true', 1, NULL)) as 'Walkins', count(IF(sessions.noshow = 'true', 1, NULL)) as 'No Shows', count(*) AS 'Total' from sessions WHERE YEAR(`date`) = YEAR(CURRENT_DATE - INTERVAL 1 YEAR) group by sessions.library";
+$result = $con->query($query);
+
+echo "<tr><th>Library</th><th>Booked</th><th>Walkins</th><th>No Shows</th><th>Total</th></tr>";
+
+foreach($result as $row){
+	echo "<tr><td>".ucfirst($row['library'])."</td><td>". $row['Booked'] . "</td><td>".$row['Walkins']."</td><td>".$row['No Shows']."</td><td>".$row['Total'] ."</td></tr>";
+}
+echo'</table></div></div>';
 ?>
 
-<div class="panel"><div class="innerPanel"><h2>Libraries</h2></div></div>
+
+
+
+
 
 </div>
 </div>
