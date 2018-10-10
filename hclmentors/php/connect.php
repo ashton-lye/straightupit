0ec15b123e0579a57eef9996ecb0b83f313a6f24
jillian <jilli-lilli@hotmail.com>
2018-10-10 13:56:53 +1300

    fixed the connect.php file again - password different for me

--- hclmentors/php/connect.php
+++ hclmentors/php/connect.php
@@ -1,6 +1,6 @@
 <?php
     try{
-		$con = new PDO('mysql:host=localhost;dbname=hcl_mentors','root','');
+		$con = new PDO('mysql:host=localhost;dbname=hcl_mentors','root','root');
 		//uni - $con = new PDO('mysql:host=mysql.cms.waikato.ac.nz;dbname=al123','al123','my11039762sql');
 		//uni - $con = new PDO('mysql:host=mysql.cms.waikato.ac.nz;dbname=','','');
 		//lappy - $con = new PDO('mysql:host=localhost;dbname=comp333-assn1-im','root','root');
